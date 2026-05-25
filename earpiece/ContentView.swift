2f0d5a9369143e14c01fa2b206e0ea4ab9d9501f
Lukas <lukas@lukasmartinson.com>
2026-05-25 01:30:08 -0400

    Initial Commit

---
+++ earpiece/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  earpiece
+//
+//  Created by Claudezone on 5/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ earpiece/earpieceApp.swift
@@ -0,0 +1,17 @@
+//
+//  earpieceApp.swift
+//  earpiece
+//
+//  Created by Claudezone on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct earpieceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
